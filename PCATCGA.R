a16667ec4ec86fb00904141d3c8f8f74578b186b
Sithspitz <rjb590@bham.ac.uk>
2018-11-26 09:30:57 +0000

    abc

---
+++ PCATCGA.R
@@ -0,0 +1,36 @@
+# Install Packages
+library("utils", "ggbiplot", "tidyverse", "tibble")
+
+# Set Working Directory (change each experiment)
+setwd("C:\\Users\\Richard\\Documents\\R\\Research 2018\\November 2018")
+
+# Check Working Directory
+getwd()
+
+# Import Luminex Data (change each experiment)
+TCGA_2014 <- read.csv("TCGA_2014_AC_Immune_Gene_Transcription.csv", stringsAsFactors = FALSE)
+
+# Check Import Structure
+str(TCGA_2014)
+
+# Assign First Column as Row Names
+rownames(TCGA_2014) <- TCGA_2014$Name
+TCGA_2014 %>% remove_rownames() %>% column_to_rownames(var ="Name")
+
+# PCA Group Assigning
+## Make sure that there are no blank recurrant columns of values
+## Don't include the first column with samples names as character string
+TCGA_2014.pca <- prcomp(TCGA_2014, center = T, scale. = T)
+summary(TCGA_2014.pca)
+
+# Assigning Samples To MT Groups
+## The number after the group represents the number of rows with this MT
+mt_groups <- c(rep("WT", 6), rep("KRAS", 2), rep("STK11", 4), rep("KRAS/STK11", 4))
+
+# Make A Plot
+ggbiplot(luminextotal.pca, ellipse = F, obs.scale = 1, var.scale = 1, choices = c(1,2), 
+         var.axes = F, labels = rownames(luminextotal), groups = mt_groups, labels.size = 1.75) + ggtitle("Luminex TCM PCA")
+         
+# Export The Plot
+ggsave('Luminex TCM PCA.pdf', last_plot())
+
